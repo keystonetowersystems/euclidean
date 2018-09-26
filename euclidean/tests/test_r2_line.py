ab88a4710e504567b6f1b12f1750ca906e752115
Greg Echelberger <greg@keystonetowersystems.com>
2018-09-26 10:15:41 -0600

    added test for line translation

--- euclidean/tests/test_r2_line.py
+++ euclidean/tests/test_r2_line.py
@@ -1,6 +1,8 @@
 import unittest
 
-from euclidean.R2.line import Line, P2
+from euclidean.R2.line import Line, P2, V2
+
+from fuzzyfloat import abs_fp as fp
 
 class LineTestCase(unittest.TestCase):
 
@@ -55,8 +57,8 @@ class LineTestCase(unittest.TestCase):
 
     def test_intersection(self):
 
-        horz = Line.ByPoints(P2(-1, 10), P2(1, 10))
-        vert = Line.ByPoints(P2(10, -1), P2(10, 1))
+        horz = Line.ByPoints(P2(fp(-1), fp(10)), P2(fp(1), fp(10)))
+        vert = Line.ByPoints(P2(fp(10), fp(-1)), P2(fp(10), fp(1)))
 
         self.assertEqual(horz.intersection(vert), P2(10, 10))
 
@@ -70,14 +72,35 @@ class LineTestCase(unittest.TestCase):
         self.assertEqual(horz.intersection(l135), P2(-10, 10))
 
     def test_closest_point(self):
-        horz = Line.ByPoints(P2(-1, 10), P2(1, 10))
+        horz = Line.ByPoints(P2(fp(-1), fp(10)), P2(fp(1), fp(10)))
 
         self.assertEqual(horz.closest(P2(0, 0)), P2(0, 10))
         self.assertEqual(horz.closest(P2(5, 10)), P2(5, 10))
 
-        vert = Line.ByPoints(P2(10, 1), P2(10, -2))
+        vert = Line.ByPoints(P2(fp(10), fp(1)), P2(fp(10), fp(-2)))
 
         self.assertEqual(vert.closest(P2(0, 5)), P2(10, 5))
         self.assertEqual(vert.closest(P2(10, 5)), P2(10, 5))
 
 
+    def test_translate(self):
+
+        line = Line(2, 1, 2)
+        self.assertEqual(line.y(0), 2)
+        self.assertEqual(line.x(0), 1)
+
+        translated = line.translate(V2(1, 1))
+        self.assertEqual(translated.y(0), 3)
+        self.assertEqual(translated.x(0), 2)
+
+        horz = Line.ByPoints(P2(-1,10), P2(1, 10))
+        self.assertEqual(horz.y(0), 10)
+
+        translated = horz.translate(V2(1,1))
+        self.assertEqual(translated.y(0), 11)
+
+        vert = Line.ByPoints(P2(10, -1), P2(10, 1))
+        self.assertEqual(vert.x(0), 10)
+
+        translated = vert.translate(V2(1, 1))
+        self.assertEqual(translated.x(0), 11)
