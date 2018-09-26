5fa56f0296873d4ab96ad225542fe4d8f9372963
Greg Echelberger <greg@keystonetowersystems.com>
2018-09-26 10:41:30 -0600

    scaffolding for divide-and-conquer convex hull replacement for jarvis march

--- euclidean/R2/polygon/hull.py
+++ euclidean/R2/polygon/hull.py
@@ -7,6 +7,18 @@ def convex_hull(points):
     return _jarvis_convex_hull(points)
 
 def _jarvis_convex_hull(points):
+    """Find the convex hull of a point cloud using the jarvis march algorithm.
+
+    Notes:
+
+        O(n * h) for n points in point cloud, h points in hull
+
+    Args:
+        points (List[P2]):
+
+    Returns:
+        (List[P2]):
+    """
     count = len(points)
     hull = []
     last_idx = 0
@@ -22,3 +34,37 @@ def _jarvis_convex_hull(points):
 
         if last_idx == 0:
             return hull
+
+def _divide_and_conquer_convex_hull(points):
+    """
+
+    Notes:
+
+        O(n * log(n))
+
+    Args:
+        points:
+
+    Returns:
+
+    """
+    count = len(points)
+    if count < 6:
+        return _jarvis_convex_hull(points)
+
+    midpoint = count // 2
+    min_cloud, max_cloud = points[:midpoint], points[midpoint:]
+
+    min_hull = _divide_and_conquer_convex_hull(min_cloud)
+    max_hull = _divide_and_conquer_convex_hull(max_cloud)
+
+    return _merge_convex_hulls(min_hull, max_hull)
+
+def _merge_convex_hulls(left_hull, right_hull):
+    pass
+
+def _min_tangent(min_hull, max_hull):
+    pass
+
+def _max_tangent(min_hull, max_hull):
+    pass
